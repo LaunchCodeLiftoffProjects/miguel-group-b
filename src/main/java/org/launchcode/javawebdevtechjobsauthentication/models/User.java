e32416cb8b2bba09eb27b9b29ae91c458eccd3e5
Happy <Mplogswdev@outlook.com>
2021-09-01 16:13:11 -0500

    bean_problem

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
@@ -1,35 +1,35 @@
-package org.launchcode.javawebdevtechjobsauthentication.models;
-
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.persistence.Entity;
-import javax.validation.constraints.NotNull;
-
-@Entity
-public class User extends AbstractEntity {
-
-    @NotNull
-    private String username;
-
-    @NotNull
-    private String pwHash;
-
-    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-    public User() {}
-
-    public User(String username, String password) {
-        this.username = username;
-        this.pwHash = encoder.encode(password);
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public boolean isMatchingPassword(String password) {
-        return encoder.matches(password, pwHash);
-    }
-
-}
+//package org.launchcode.javawebdevtechjobsauthentication.models;
+//
+//
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//import javax.persistence.Entity;
+//import javax.validation.constraints.NotNull;
+//
+//@Entity
+//public class User extends AbstractEntity {
+//
+//    @NotNull
+//    private String username;
+//
+//    @NotNull
+//    private String pwHash;
+//
+//    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//
+//    public User() {}
+//
+//    public User(String username, String password) {
+//        this.username = username;
+//        this.pwHash = encoder.encode(password);
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public boolean isMatchingPassword(String password) {
+//        return encoder.matches(password, pwHash);
+//    }
+//
+//}
