6b7471570bc258b637d0b2f8858ed7fc42159cf3
Happy <Mplogswdev@outlook.com>
2021-09-01 22:15:37 -0500

    thymeleaf_extras

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/users/WebSecurityConfig.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/users/WebSecurityConfig.java
@@ -41,6 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll()
