cf58111cd073b33e3909e2de9ccc4487e3b67f0b
MBeckett <maddbeck@gmail.com>
2021-09-04 15:13:30 -0500

    Modified cart service, added Cart Rest Controller

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/CartRestController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/CartRestController.java
@@ -1,8 +1,21 @@
 package org.launchcode.javawebdevtechjobsauthentication.controllers;
 
+import org.launchcode.javawebdevtechjobsauthentication.services.CartServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CartRestController {
+
+    @Autowired
+    private CartServices cartServices;
+
+    @PostMapping("/cart/add/{productId}/{quantity}")
+    public String addProductToCart(@PathVariable int productId, @PathVariable int quantity){
+        int addedQuantity = cartServices.addProductQuantity(productId, quantity);
+        return addedQuantity + " item(s) added to cart.";
+    }
 
 }
