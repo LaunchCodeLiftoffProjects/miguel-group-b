cdef079fa2bf0e53f85c28d8ae8fcaf0198c21a4
MBeckett <maddbeck@gmail.com>
2021-09-07 15:27:49 -0500

    Added Product thymeleaf templates

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/ProductController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/ProductController.java
@@ -1,11 +1,14 @@
 package org.launchcode.javawebdevtechjobsauthentication.controllers;
 
+import org.launchcode.javawebdevtechjobsauthentication.models.Product;
 import org.launchcode.javawebdevtechjobsauthentication.models.data.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("products")
@@ -21,4 +24,30 @@ public class ProductController {
         return "products/index";
     }
 
+    @GetMapping("add")
+    public String displayAddProductForm(Model model){
+        model.addAttribute(new Product());
+        return "products/add";
+    }
+
+    @PostMapping("add")
+    public String processAddProductForm(@ModelAttribute Product newProduct, Errors errors){
+        if(errors.hasErrors()){
+            return "products/add";
+        }
+        productRepository.save(newProduct);
+        return "redirect:";
+    }
+
+    @GetMapping("view/{productId}")
+    public String viewProductById(Model model, @PathVariable int productId){
+        Optional optItem = productRepository.findById(productId);
+        if(optItem.isPresent()){
+            Product product = (Product) optItem.get();
+            model.addAttribute("product", product);
+            return "products/view";
+        } else {
+            return "redirect:";
+        }
+    }
 }
