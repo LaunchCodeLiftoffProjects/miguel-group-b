02131e7e4c2a1d709e77b61f3e6d2db5146863fd
MBeckett <maddbeck@gmail.com>
2021-09-07 13:28:42 -0500

    default changelist

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/CartRestController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/CartRestController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +27,7 @@ public class CartRestController {
         List<Product> products = new ArrayList<>();
         products = productService.viewAllProducts();
         model.addAttribute("products", productService.viewAllProducts());
-        return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
     @PostMapping("/cart/add/{productId}/{quantity}")
