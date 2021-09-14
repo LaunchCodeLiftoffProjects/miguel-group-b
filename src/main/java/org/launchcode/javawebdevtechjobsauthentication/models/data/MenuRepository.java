4a5083ccda98224e5cf4c5edf514050780e9d5bd
Happy <Mplogswdev@outlook.com>
2021-09-13 20:09:13 -0500

    loation_add_workin

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/MenuRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/MenuRepository.java
@@ -1,7 +1,7 @@
-package org.launchcode.javawebdevtechjobsauthentication.models.data;
-
-import org.launchcode.javawebdevtechjobsauthentication.vendors.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<Menu, Integer> {
-}
+//package org.launchcode.javawebdevtechjobsauthentication.models.data;
+//
+//import org.launchcode.javawebdevtechjobsauthentication.vendors.Menu;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface MenuRepository extends JpaRepository<Menu, Integer> {
+//}
