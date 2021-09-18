41fe3ab3e7256233e940d0286e0cb3b37f04b550
MBeckett <maddbeck@gmail.com>
2021-09-18 17:37:22 -0500

    Added Font Awesome CDN to fragments, modified navbar

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/CartController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/CartController.java
@@ -35,7 +35,7 @@ public class CartController {
             cartService.findBySessionToken(sessionToken);
             cartService.addToExistingCart(id,sessionToken, quantity);
         }
-        return "redirect:/";
+        return "redirect:/cart";
     }
 
 //    @GetMapping("/cart")
