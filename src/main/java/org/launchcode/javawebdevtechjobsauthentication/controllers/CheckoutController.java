bce61aa3582056ec10a3684ff040e2eb1f57b65e
MBeckett <maddbeck@gmail.com>
2021-09-21 18:39:57 -0500

    Created checkout controller

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/CheckoutController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/CheckoutController.java
@@ -4,13 +4,26 @@ import org.launchcode.javawebdevtechjobsauthentication.models.Cart;
 import org.launchcode.javawebdevtechjobsauthentication.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class CheckoutController {
 
     @Autowired
     private CartService cartService;
+
+    @PostMapping("/processPayment")
+    public String processUserPayment(HttpServletRequest request, Model model, @RequestParam Cart newCart){
+        String sessionToken = (String) request.getSession(true).getAttribute("sessionToken");
+        Cart cart = cartService.findBySessionToken(sessionToken);
+        cart.getTotalCost();
+
+        return "redirect:/cart";
+    }
 
     public String payCartTotal(@RequestParam Cart newCart ,String sessionToken){
         Cart cart = cartService.findBySessionToken(sessionToken);
